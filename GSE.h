7594d7339c1fa528ec6a10c64399613f95f12c59
Vrushabh27 <vrushabh27@gmail.com>
2021-07-05 17:53:07 +0530

    Update GSE.h

--- GSE.h
+++ GSE.h
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 /*********************************************************************
 * Software License Agreement (BSD License)
 *
-*  Copyright (c) 2008, Willow Garage, Inc.
+*  Copyright (c) 2021, IIT Madras.
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
@@ -15,7 +14,7 @@
 *     copyright notice, this list of conditions and the following
 *     disclaimer in the documentation and/or other materials provided
 *     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
+*   * Neither the name of the IIT Madras nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
 *
@@ -33,48 +32,11 @@
 *  POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************/
 
-/* Author: Nikhil P, Vrushabh Zinage */
+/* Author: Nikhil P, Vrushabh Zinage, Adhvaith Ramkumar and Satadal Ghosh */
 
 #ifndef OMPL_GEOMETRIC_PLANNERS_GSE_GSE_
 #define OMPL_GEOMETRIC_PLANNERS_GSE_GSE_
 
-=======
- /*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, Indian Institute of Technology Madras
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Stanford University nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-  
- /* Authors: Nikhil Prakash (IIT Madras) */
- /* Algorithm design: Vrushabh Zinage and Satadal Ghosh (IIT Madras) */
 
 #include <vector>
 #include <memory>
